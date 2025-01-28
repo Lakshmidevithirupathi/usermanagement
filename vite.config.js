a3ce0ea3fc6a054b34a4a59509b2b513bf1162d4
Lakshmidevi <lakshmitirupathi366@gmail.com>
2025-01-28 18:06:38 +0530

    Updated vite.config.js with new configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// "https:vite.dev/config/"
+
 export default defineConfig({
+
+  build: {
+    outDir: 'build', // make sure this is set to 'build'
+  },
   plugins: [react()],
+  base:"/usermanagement/"
 })
